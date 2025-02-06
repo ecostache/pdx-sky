3ba07c2aeef35052c3ee0ea1100e79120328c475
Emanuel Costache <ecostache@gmail.com>
2025-02-05 20:33:08 -0800

    Add site analytics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { IBM_Plex_Sans } from 'next/font/google';
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const plexSans = IBM_Plex_Sans({
   weight: ['100', '200', '300', '400', '500', '600', '700'],
